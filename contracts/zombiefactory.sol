a71833a13e08417531144d008b271b6311b91285
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-10-17 20:25:54 -0700

    Uplifted code to 0.7.6

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,13 +1,14 @@
-pragma solidity ^0.4.25;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
 
 import "./ownable.sol";
-import "./safemath.sol";
+//import "./safemath.sol";
 
 contract ZombieFactory is Ownable {
 
-  using SafeMath for uint256;
-  using SafeMath32 for uint32;
-  using SafeMath16 for uint16;
+  //using SafeMath for uint256;
+  //using SafeMath32 for uint32;
+  //using SafeMath16 for uint16;
 
   event NewZombie(uint zombieId, string name, uint dna);
 
@@ -29,19 +30,22 @@ contract ZombieFactory is Ownable {
   mapping (uint => address) public zombieToOwner;
   mapping (address => uint) ownerZombieCount;
 
-  function _createZombie(string _name, uint _dna) internal {
-    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+  function _createZombie(string memory _name, uint _dna) internal {
+    //uint id = zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime), 0, 0)) - 1;
+	zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime), 0, 0));
+	uint id = zombies.length -1;
     zombieToOwner[id] = msg.sender;
-    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
-    emit NewZombie(id, _name, _dna);
+    //ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+    ownerZombieCount[msg.sender] += 1;
+	emit NewZombie(id, _name, _dna);
   }
 
-  function _generateRandomDna(string _str) private view returns (uint) {
+  function _generateRandomDna(string memory _str) private view returns (uint) {
     uint rand = uint(keccak256(abi.encodePacked(_str)));
     return rand % dnaModulus;
   }
 
-  function createRandomZombie(string _name) public {
+  function createRandomZombie(string memory _name) public {
     require(ownerZombieCount[msg.sender] == 0);
     uint randDna = _generateRandomDna(_name);
     randDna = randDna - randDna % 100;
