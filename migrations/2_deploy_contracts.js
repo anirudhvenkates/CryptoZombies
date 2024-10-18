8aec100d22b3932e2bd691ee550ec1598a0f91a8
EvanHei <117128641+EvanHei@users.noreply.github.com>
2024-10-18 00:07:57 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-var safemath = artifacts.require("./safemath.sol");
 var zombiefactory = artifacts.require("./zombiefactory.sol");
 var zombiefeeding = artifacts.require("./zombiefeeding.sol");
 var zombiehelper = artifacts.require("./zombiehelper.sol");
@@ -7,7 +6,6 @@ var zombieownership = artifacts.require("./zombieownership.sol");
 
 module.exports = function(deployer) {
 
-    deployer.deploy(safemath);
     deployer.deploy(zombiefactory);
     deployer.deploy(zombiefeeding);
     deployer.deploy(zombiehelper);
