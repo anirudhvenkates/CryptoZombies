edc7a8f72bb2dddd65e68eef179f65c12c3da7c7
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-10-18 20:53:16 -0700

    Fixed an issue with skipDryRun being false by default

--- write_config.py
+++ write_config.py
@@ -8,14 +8,14 @@ config_data = {
         "network_id": input("Enter Network ID (default: 5777): ") or "5777"
     },
     "sepolia": {
-        "provider": input("Enter Provider URL: (default: https://sepolia.infura.io/v3/ad90e0f9dbec4a92a6ba74831d62525e)") or "https://sepolia.infura.io/v3/ad90e0f9dbec4a92a6ba74831d62525e",
+        "provider": input("Enter Provider URL: (default: https://sepolia.infura.io/v3/ad90e0f9dbec4a92a6ba74831d62525e): ") or "https://sepolia.infura.io/v3/ad90e0f9dbec4a92a6ba74831d62525e",
         "timeout": int(input("Enter Timeout (default: 40000): ") or 40000),
         "network_id": int(input("Enter Network ID (default: 11155111): ") or 11155111),
         "gas": int(input("Enter Gas (default: 5000000): ") or 5000000),
         "gasPrice": int(input("Enter Gas Price (default: 1000000000): ") or 1000000000),
         "confirmations": int(input("Enter Confirmations (default: 2): ") or 2),
         "timeoutBlocks": int(input("Enter Timeout Blocks (default: 200): ") or 200),
-        "skipDryRun": input("Skip Dry Run (true/false, default: true): ").lower() == 'true',
+        "skipDryRun": 'true' and input("Skip Dry Run (true/false, default: true): ").lower() == '',
         "fromAccount": input("Enter From Account: (default: 0x647A17644F560e71e9A1BbC317ba3ba736C3369F)") or "0x647A17644F560e71e9A1BbC317ba3ba736C3369F"
     },
     "solidity": {
