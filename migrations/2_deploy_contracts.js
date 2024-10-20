105e98aab4d568ac46fd1563c450353406b16a62
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-10-19 18:28:14 -0700

    Added kitcontract to be deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,11 @@ var zombiefeeding = artifacts.require("./zombiefeeding.sol");
 var zombiehelper = artifacts.require("./zombiehelper.sol");
 var zombieattack = artifacts.require("./zombieattack.sol");
 var zombieownership = artifacts.require("./zombieownership.sol");
+var kittycontract = artifacts.require("./kittycontract.sol");
 
 module.exports = function(deployer) {
 
+	deployer.deploy(kittycontract);
     deployer.deploy(zombiefactory);
     deployer.deploy(zombiefeeding);
     deployer.deploy(zombiehelper);
